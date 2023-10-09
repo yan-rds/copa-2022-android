32a5395ac1afa19fc32fd9a21e850f2e90efa0a6
Yan <yyan1207@gmail.com>
2023-10-09 14:19:35 -0300

    Implementing match list layout

---
+++ app/src/main/kotlin/me/dio/copa/catar/features/MainActivity.kt
@@ -0,0 +1,27 @@
+package me.dio.copa.catar.features
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import dagger.hilt.android.AndroidEntryPoint
+import me.dio.copa.catar.ui.theme.Copa2022Theme
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    private val viewModel by viewModels<MainViewModel>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Copa2022Theme {
+                val state by viewModel.state.collectAsState()
+                MainScreen(matches = state.matches)
+
+            }
+        }
+    }
+
+}
